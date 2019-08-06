c1bb1f9af286efbe50889887ca76b6867b83ae25
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-08-06 10:32:22 -0400

    It may be that the testing won't work right
    At least not until I can figure out how to trick the adapter into using
    firebase's testing apparatus.

--- test/index.js
+++ test/index.js
@@ -2,6 +2,12 @@ const testAdapter = require('fortune/test/adapter')
 const adapter = require('../lib')
 
 testAdapter(adapter, {
-  url: 'mongodb://localhost:27017/test',
+  projectId: 'fortune-firestore-unit-tests',
+  credentials: {
+    client_email: "fortune-firestore@example.com",
+    private_key: "private_key"
+  },
+  apiEndpoint: 'http://localhost:8080',
+  namespace: 'fortune-adapter-test',
   generateId: () => Math.floor(Math.random() * Math.pow(2, 32)).toString(16)
 })
