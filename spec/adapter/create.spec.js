c68e30741614844db768a9d9eeec10b809f27b2e
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-08-10 20:27:43 -0400

    GET MY FIRST REAL PASSING TEST.

--- spec/adapter/create.spec.js
+++ spec/adapter/create.spec.js
@@ -1,23 +1,27 @@
 require("../spec-helper");
 const { expect } = require("chai");
-const { setup, teardown } = require("../helpers");
+const { setup, teardown, buildAdapter } = require("../helpers");
 const { records } = require("../fixtures");
 
 describe("Fortune Firestore Adapter", function() {
   describe("#create(type, records, [meta])", async function() {
+    let adapter;
     let db;
-    let ref;
 
     before(async function() {
+      adapter = await buildAdapter();
       db = await setup(null, records);
-      ref = db.collection("users");
     });
 
     after(async function() {
       await teardown();
     });
 
-    it("no-ops when records is an empty array");
+    it("no-ops when records is an empty array", async function() {
+      const records = await adapter.create("user", []);
+      expect(records).to.deep.equal([]);
+    });
+
     it("maintains type when creating new records");
     it("does not allow using duplicate ids");
     it("generates an id when none is given");
