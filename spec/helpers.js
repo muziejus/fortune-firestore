13c01bf922edda28f1dd5ab52717c43557b7378a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-08-10 20:24:50 -0400

    Build out a buildAdapter function.

--- spec/helpers.js
+++ spec/helpers.js
@@ -1,13 +1,44 @@
-/* eslint no-await-in-loop: 0 */
+/* eslint no-await-in-loop: 0, camelcase: 0 */
 const fs = require("fs");
 const firebase = require("@firebase/testing");
+const message = require("fortune/lib/common/message");
+const AdapterSingleton = require("fortune/lib/adapter/singleton");
+const FirestoreAdapter = require("../lib");
+const { recordTypes } = require("./fixtures");
 
 const projectId = "fortune-firestore-spec";
 const port = 8080;
 const coverageUrl = `http://localhost:${port}/emulator/v1/projects/${projectId}:ruleCoverage.html`;
 
+module.exports.buildAdapter = async () => {
+  let adapter;
+  try {
+    adapter = new AdapterSingleton({
+      recordTypes,
+      message,
+      adapter: [
+        FirestoreAdapter,
+        {
+          projectId: "fortune-firestore-unit-tests",
+          credentials: {
+            client_email: "fortune-firestore@example.com",
+            private_key: "private_key"
+          }
+        }
+      ]
+    });
+  } catch (error) {
+    return Promise.reject(error);
+  }
+
+  return adapter;
+};
+
 module.exports.setup = async (auth, data) => {
-  const rules = fs.readFileSync("./spec/firestore-files/firestore.rules", "utf8");
+  const rules = fs.readFileSync(
+    "./spec/firestore-files/firestore.rules",
+    "utf8"
+  );
   const app = await firebase.initializeTestApp({
     projectId,
     auth
