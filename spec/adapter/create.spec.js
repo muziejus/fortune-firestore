c825fd34395a6a44a919446f21b787528eaf635e
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-08-15 16:46:25 -0400

    feat: Implement Adapter#create by having it pass all tests and lint index.

--- spec/adapter/create.spec.js
+++ spec/adapter/create.spec.js
@@ -1,20 +1,23 @@
 require("../spec-helper");
 const { expect } = require("chai");
-const { setupDB, teardown, buildAdapter } = require("../helpers");
+const keys = require("fortune/lib/common/keys");
+const { ConflictError } = require("fortune/lib/common/errors");
+const { testIds, setupDB, teardown, buildAdapter } = require("../helpers");
 const { records } = require("../fixtures");
 
 describe("Fortune Firestore Adapter", function() {
   describe("#create(type, records, [meta])", async function() {
     let db;
     let adapter;
+    const primaryKey = keys.primary;
 
-    before(async function() {
+    beforeEach(async function() {
       db = await setupDB(null, records);
       adapter = await buildAdapter();
       await adapter.connect();
     });
 
-    after(async function() {
+    afterEach(async function() {
       await teardown(db);
     });
 
@@ -23,10 +26,47 @@ describe("Fortune Firestore Adapter", function() {
       expect(records).to.deep.equal([]);
     });
 
-    it("maintains type when creating new records");
-    it("does not allow using duplicate ids");
-    it("generates an id when none is given");
-    it("returns the records in the same order as they are created");
+    it("maintains type when creating new records", async function() {
+      const deadbeef = Buffer.from("deadbeef", "hex");
+      const date = new Date();
+      const records = await adapter.create("user", [
+        {
+          id: 3,
+          picture: deadbeef,
+          birthday: date
+        }
+      ]);
+      expect(records[0].picture).to.deep.equal(deadbeef);
+      expect(Math.abs(records[0].birthday.getTime() - date.getTime())).to.be.lt(
+        1000
+      );
+    });
+
+    it("does not allow using duplicate ids", async function() {
+      let records = await adapter.find("user");
+      expect(records).to.have.length(2);
+      expect(adapter.create("user", [{ id: 1 }])).to.be.rejectedWith(ConflictError);
+    });
+
+    it("generates an id when none is given", async function() {
+      let records = await adapter.create("user", [{ name: "joe" }]);
+      const id = records[0][primaryKey];
+      expect(testIds([id])).to.have.length(1);
+      expect(records[0].picture).to.be.null;
+      expect(records[0].nicknames).to.deep.equal([]);
+      records = await adapter.find("user", [id]);
+      expect(records).to.have.length(1);
+      expect(records[0][primaryKey]).to.equal(id);
+      expect(testIds([id])).to.have.length(1);
+    });
+
+    it("returns the records in the same order as they are created", async function() {
+      const records = await adapter.create("user", [
+        { name: "a" },
+        { name: "b" },
+        { name: "c" }
+      ]);
+      expect(records.map(record => record.name)).to.deep.equal(["a", "b", "c"]);
+    });
   });
 });
-
